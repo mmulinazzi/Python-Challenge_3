916998f0671a3a91c79e5bea79449a3f6633ead7
Matt <busydad6@gmail.com>
2018-10-09 23:22:00 -0500

    cleaned up my file

--- My Homework_3B.py
+++
@@ -1,5 +0,0 @@
-import os
-import csv
-with open('Resources/election_data.csv') as csv_file:
-    csv_read=csv.reader(csv_file,delimiter=',')
-
---
+++ MyHomework_3B.py
@@ -0,0 +1,5 @@
+import os
+import csv
+with open('Resources/election_data.csv') as csv_file:
+    csv_read=csv.reader(csv_file,delimiter=',')
+
